d9e2fbb39282acb582563708b639114134e6d24e
paulzolotov <zolotov10pl@gmail.com>
2022-11-03 14:50:13 +0300

    add hw_lesson3

---
+++ hw_lesson 3/ex4/main.py
@@ -0,0 +1,9 @@
+#! /usr/local/bin/python3
+
+while 1:
+	some_str = input('Введите предложение: ')
+	s = input('Введите символ: ')
+	count_s = some_str.count(s)
+	print('Символ ', s, ' встречается ', count_s, ' раз/а')
+	
+
