c0471cf1c44c02f62bd85bfa7d2755ae0568a9e3
paulzolotov <zolotov10pl@gmail.com>
2022-11-11 09:59:18 +0300

    correct hw4_ex4(3p)

--- hw_lesson 4/ex4/main.py
+++ hw_lesson 4/ex4/main.py
@@ -6,5 +6,5 @@ while 1:
     str3 = input('Введите третье слово: ')
     print('1. Буквы которые встречаются во всех словах: ', set(str1) & set(str2) & set(str3))
     print('2. Все буквы, которые хотя бы раз встречаются в слове: ', set(str1) | set(str2) | set(str3))
-    print('3. Буквы в каждом слове которые не встречаются в других словах: ', set(str1) ^ set(str2) ^ set(str3))
+    print('3. Буквы в каждом слове которые не встречаются в других словах: ', (set(str1) - set(str2) - set(str3)) | (set(str2) - set(str1) - set(str3)) | (set(str3) - set(str1) - set(str2)))
 
