f6f380037cc39b689e3e71486f058a570d1018b6
paulzolotov <zolotov10pl@gmail.com>
2022-12-15 12:04:30 +0300

    add ex2 in hw15

---
+++ hw_lesson 15/main_ex2.py
@@ -0,0 +1,46 @@
+"""
+Используя ORM peewee (https://pypi.org/project/peewee/) создайте функцию,
+которая получает от пользователя название альбома через input и выводит список всех треков в этом альбоме.
+"""
+
+from peewee import *
+
+db = SqliteDatabase('chinook.db')
+
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+
+class Albums(BaseModel):
+    title = CharField(column_name='Title', null=False)
+    album_id = AutoField(column_name='Albumid')
+
+    class Meta:
+        table_name = 'albums'
+
+
+class Tracks(BaseModel):
+    name = CharField(column_name='Name', null=False)
+    album_id = AutoField(column_name='Albumid')
+
+    class Meta:
+        table_name = 'tracks'
+
+
+def get_tracks():
+    album_name = input('Введите название альбома: ')  # A-Sides
+    with db:
+        return Tracks.select().\
+            where(Tracks.album_id == Albums.select().
+                  where(Albums.title == album_name))
+
+
+all_tracks = get_tracks()
+for track in all_tracks:
+    print(track.name)
+
+
+
+
