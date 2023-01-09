d109f37124428e516ae3dfecd8c92854e32a091e
paulzolotov <zolotov10pl@gmail.com>
2023-01-09 09:33:36 +0300

    add initial flask app from hw18

---
+++ hw_lesson 19/flask_app/app1/view.py
@@ -0,0 +1,20 @@
+from flask import Blueprint
+import requests
+import datetime
+
+app1 = Blueprint('something', __name__)
+
+
+@app1.route('/index')
+def index():
+    return '<em>Hello from Flask server</em>'
+
+
+@app1.route('/time')
+def time_iso8601():
+    return 'Current time: ' + datetime.datetime.now().time().isoformat()
+
+
+@app1.route('/quote')
+def quote():
+    return requests.get('https://api.kanye.rest').json()['quote'] + '<br>Ⓒ Kanye West'
