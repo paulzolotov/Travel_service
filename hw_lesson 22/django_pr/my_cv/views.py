c03359ec745d946abc1637b6af9d68efa53e1daa
paulzolotov <zolotov10pl@gmail.com>
2023-01-16 12:28:13 +0300

    add base django project without information

--- hw_lesson 22/django_pr/my_cv/views.py
+++ hw_lesson 22/django_pr/my_cv/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest, JsonResponse, QueryDict
+from django.views.decorators.http import require_http_methods
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import View, TemplateView
 
-# Create your views here.
+
+def fun_index(request: HttpRequest):
+    return render(request, 'my_cv/index.html', {})
+
+
+def fun_skills(request: HttpRequest):
+    return render(request, 'my_cv/skills.html', {})
+
+
+def fun_edu(request: HttpRequest):
+    return render(request, 'my_cv/edu.html', {})
+
+
+@csrf_exempt
+@require_http_methods(['POST'])
+def fun_sign(request: HttpRequest):
+    return request.POST.dict()
