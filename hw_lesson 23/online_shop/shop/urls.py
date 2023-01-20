01ecb1a3698c7d7bb43140d91c2173055148aa29
paulzolotov <zolotov10pl@gmail.com>
2023-01-20 16:23:25 +0300

    add urls in html files and modified styles

--- hw_lesson 23/online_shop/shop/urls.py
+++ hw_lesson 23/online_shop/shop/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
 
+app_name = 'shop'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('games/', views.index, name='games'),
+    path('?order_by=name', views.index, name='re-index'),
     path('categories/', views.categories, name='categories'),
-    path('game-<slug:game_slug>/', views.get_game, name='game'),
-    path('category-<slug:category_slug>/', views.get_category, name='category')
+    path('game/<slug:game_slug>/', views.get_game, name='game'),
+    path('<slug:category_slug>/', views.get_category, name='category')
 ]
