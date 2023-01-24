b722da769d42558fde2a0de2cb8e5f47fd44cced
paulzolotov <zolotov10pl@gmail.com>
2023-01-24 19:53:11 +0300

    init project from homework23

---
+++ hw_lesson 24/online_shop/shop/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpRequest, HttpResponse
+from .models import Game, Category
+from decimal import Decimal
+
+
+# Create your views here.
+def order_index(request: HttpRequest, order_by='without'):
+    print(order_by)
+    sorting_dict = {
+        'price': Game.objects.filter(is_active=True).order_by('price').all(),
+        'name': Game.objects.filter(is_active=True).order_by('name').all(),
+        'without': Game.objects.filter(is_active=True).all()
+    }
+    games = sorting_dict.get(order_by)
+    return render(request, 'shop/games_home_page.html', {'games': games})
+
+
+def categories(request: HttpRequest):
+    categories = Category.objects.filter(is_active=True).all()
+    return render(request, 'shop/categories.html', context={'categories': categories})
+
+
+def get_game(request: HttpRequest, *args, **kwargs):
+    game_slug = kwargs.get('game_slug')
+    game = get_object_or_404(Game, slug=game_slug)
+    print(game.game_image.url)
+    return render(request, 'shop/game_page.html', context={'game': game})
+
+
+def get_category(request: HttpRequest, *args, **kwargs):
+    category_slug = kwargs.get('category_slug')
+    category = get_object_or_404(Category, slug=category_slug)
+    games_from_category = Game.objects.all().filter(category=category)
+    print(games_from_category)
+    return render(request, 'shop/category_page.html', context={'games_from_category': games_from_category,
+                                                       'category': category})
