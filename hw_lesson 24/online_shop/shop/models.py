73c58edd4723b3e8d31daa88bd4f8e9d5fc4840e
paulzolotov <zolotov10pl@gmail.com>
2023-01-26 19:10:41 +0300

    add abstract model

--- hw_lesson 24/online_shop/shop/models.py
+++ hw_lesson 24/online_shop/shop/models.py
@@ -1,13 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+class ShopInfoMixin(models.Model):
+    slug = models.SlugField(max_length=50, verbose_name='Short Name')
+    is_active = models.BooleanField(default=True, verbose_name='Is it active?')
+
+    class Meta:
+        abstract = True
 
 
-class Category(models.Model):
+class Category(ShopInfoMixin):
     title = models.CharField(max_length=100, verbose_name='Category Title')
-    slug = models.SlugField(max_length=50, verbose_name='Category Slug')
     description = models.TextField(verbose_name='Category Description')
-    is_active = models.BooleanField(default=True, verbose_name='Category is active?')
     games_amount = models.IntegerField(default=0, verbose_name='Games Amount In Category')
 
     @classmethod
@@ -29,17 +34,15 @@ class Category(models.Model):
         return f"{self.title}"
 
 
-class Game(models.Model):
+class Game(ShopInfoMixin):
     name = models.CharField(max_length=100, verbose_name='Game Name')
     pub_date = models.DateTimeField(auto_now_add=True, verbose_name='Game publication date')
     release_date = models.DateTimeField(auto_now_add=False, verbose_name='Release date')
     price = models.DecimalField(verbose_name='Game Price', max_digits=5, max_length=4, decimal_places=2)
-    slug = models.SlugField(max_length=50, verbose_name='Game Slug')
     category = models.ForeignKey(Category, verbose_name='Game Category', on_delete=models.SET_DEFAULT,
                                  default=Category.get_default_category_pk, null=True)
     description = models.TextField(verbose_name='Game Description')
     game_image = models.ImageField(verbose_name='Game Image', upload_to='shop')
-    is_active = models.BooleanField(default=True, verbose_name='Game is active?')
 
     class Meta:
         verbose_name = 'Game'
