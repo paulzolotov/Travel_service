bc3f826be1a0588f41e990e0cdd2d7dbe7cc152e
paulzolotov <zolotov10pl@gmail.com>
2023-02-10 16:20:28 +0300

    add ex1 in ex29

--- hw_lesson 24/online_shop/users/forms.py
+++ hw_lesson 24/online_shop/users/forms.py
@@ -1,12 +1,28 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
+from django.forms import ValidationError
 
 
 class CustomUserCreationForm(UserCreationForm):
     username = forms.CharField(label='Username', widget=forms.TextInput(attrs={'class': 'form-input'}))
     email = forms.EmailField(label='Email address', widget=forms.EmailInput(attrs={'class': 'form-input'}))
     password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
-    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput(
+        attrs={'class': 'form-input'}))
 
     class Meta(UserCreationForm.Meta):
         fields = UserCreationForm.Meta.fields + ("email",)
+
+
+class CustomPasswordChangeForm(PasswordChangeForm):
+    old_password = forms.CharField(label='Old Password', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+    new_password1 = forms.CharField(label='New Password', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+    new_password2 = forms.CharField(label='New Password confirmation', widget=forms.PasswordInput(
+        attrs={'class': 'form-input'}))
+
+    def clean(self):
+        cleaned_data = super().clean()
+        user = self.user
+        new = cleaned_data.get('new_password1')
+        if user.check_password(new):
+            raise ValidationError('The new password is the same as the old one.')
--- hw_lesson 24/online_shop/users/urls.py
+++ hw_lesson 24/online_shop/users/urls.py
@@ -1,13 +1,24 @@
-from django.urls import path, include
+from django.urls import path, include, reverse_lazy
 
 from django.contrib.auth import views as auth_views
 from . import views
+from . import forms
 
 app_name = 'users'
 
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('register/', views.register, name='register'),
-    path('login/', auth_views.LoginView.as_view(next_page='shop:index', template_name='users/login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(next_page='shop:index', template_name='users/login.html'),
+         name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='shop:index'), name='logout'),
+    path('change-pass/', auth_views.PasswordChangeView.as_view(
+        success_url=reverse_lazy('users:password_change_done'),  # Куда буду перенаправлен после успешной смены пароля,
+        # можно перенаправить и на главную страницу. Указан стандартный, переопределил из-за app_name = 'users'.
+        template_name='users/password_change.html',  # шаблон, использующейся для отображения
+        form_class=forms.CustomPasswordChangeForm),  # форма, которая передается в шаблон
+        name='change'),
+    path('change-pass-confirm/', auth_views.PasswordChangeDoneView.as_view(
+        template_name='users/password_change_success.html'),
+        name='password_change_done')
 ]
