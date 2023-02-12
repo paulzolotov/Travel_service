d0c774b30b477cc87e323f6d8d4978d08d92d084
paulzolotov <zolotov10pl@gmail.com>
2023-02-12 22:23:21 +0300

    add some files in hw29

--- hw_lesson 24/online_shop/online_shop/settings.py
+++ hw_lesson 24/online_shop/online_shop/settings.py
@@ -40,6 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.github'
 ]
 
 MIDDLEWARE = [
--- hw_lesson 24/online_shop/users/urls.py
+++ hw_lesson 24/online_shop/users/urls.py
@@ -27,5 +27,13 @@ urlpatterns = [
         success_url=reverse_lazy('users:pass-reset-done')),
         name='pass-reset'),
     path('password-reset-done', auth_views.PasswordResetDoneView.as_view(
-        template_name="users/password_reset_done.html"),
-        name="pass-reset-done")]
+        template_name='users/password_reset_done.html'),
+        name='pass-reset-done'),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        success_url=reverse_lazy('users:reset_complete'),
+        template_name='users/password_reset_confirm.html'),
+        name='password_reset_confirm'),
+    path('password-reset-complete', auth_views.PasswordResetCompleteView.as_view(
+        template_name='users/password_reset_complete.html'),
+        name='reset_complete'),
+    ]
