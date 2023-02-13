873f94da1d3940fc0db75c96660a9d0f119ddbe9
paulzolotov <zolotov10pl@gmail.com>
2023-02-13 16:33:57 +0300

    add ex2 in hw29

--- hw_lesson 24/online_shop/online_shop/settings.py
+++ hw_lesson 24/online_shop/online_shop/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
@@ -143,3 +144,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "localhost"
 EMAIL_PORT = 1025
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',  # Оставляет возможность входа по логину и паролю.
+    # Стандартная модель пользователя.
+    'allauth.account.auth_backends.AuthenticationBackend'  # Модель пользователя, вошедшего через социальную сеть.
+]
+
+SITE_ID = 1
+ACCOUNT_EMAIL_VERIFICATION = "none"  # Нужно ли отправлять письмо, что прошла успешная верификация аккаунта
+LOGIN_REDIRECT_URL = 'shop:index'  # Ресурс для перенаправления после успешной авторизации.
