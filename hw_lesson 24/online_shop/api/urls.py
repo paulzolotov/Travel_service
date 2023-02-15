2eae64ca6f06af9ad113041718a0a687165373b2
paulzolotov <zolotov10pl@gmail.com>
2023-02-15 17:07:57 +0300

    add ex4 in hw30

--- hw_lesson 24/online_shop/api/urls.py
+++ hw_lesson 24/online_shop/api/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('games/<str:some_value>', views.GetGameInfoView.as_view()),
+    path('games/', views.GetGameInfoView.as_view()),
 ]
--- hw_lesson 24/online_shop/api/views.py
+++ hw_lesson 24/online_shop/api/views.py
@@ -5,6 +5,7 @@ from rest_framework.renderers import BrowsableAPIRenderer, JSONRenderer
 
 from shop.models import Game
 from .serializers import GameSerializer
+import random
 
 
 # Create your views here.
@@ -13,8 +14,16 @@ class GetGameInfoView(APIView):
 
     renderer_classes = [BrowsableAPIRenderer, JSONRenderer]
 
-    def get(self, request, some_value):
-        print(some_value)
-        queryset = Game.objects.all()
-        serializer_for_queryset = GameSerializer(instance=queryset, many=True)
+    def get(self, request):
+        if self.get_queryset() == 'true' or self.get_queryset() == 'True':
+            all_games = Game.objects.all()
+            random_game = random.randint(0, all_games.count()-1)
+            queryset = all_games[random_game]
+            serializer_for_queryset = GameSerializer(instance=queryset)
+        else:
+            queryset = Game.objects.all()
+            serializer_for_queryset = GameSerializer(instance=queryset, many=True)
         return Response({"games": serializer_for_queryset.data})
+
+    def get_queryset(self):
+        return self.request.query_params.get('random', None)
