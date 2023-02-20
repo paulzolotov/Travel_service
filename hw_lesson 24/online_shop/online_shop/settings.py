4942da674d190b220e7918b06c1716c09eb7af50
paulzolotov <zolotov10pl@gmail.com>
2023-02-20 12:16:07 +0300

    add ex1 in hw31

--- hw_lesson 24/online_shop/online_shop/settings.py
+++ hw_lesson 24/online_shop/online_shop/settings.py
@@ -105,6 +105,9 @@ REST_FRAMEWORK = {
     'UNICODE_JSON': True,
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 2
 }
 
 
