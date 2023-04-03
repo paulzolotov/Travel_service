b12ce0268a696887d5cf90041ddb0458948864b9
paulzolotov <zolotov10pl@gmail.com>
2023-04-03 16:51:09 +0300

    add TimeTrip model in booking

--- final_edu_project/travel_service/booking/admin.py
+++ final_edu_project/travel_service/booking/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Direction, DateRoute, TimeTrip
 
 # Register your models here.
+
+
+@admin.register(Direction)
+class DirectionAdmin(admin.ModelAdmin):
+    """Класс для отображения на панели администратора информации о конкретном Направлении маршрута."""
+
+    list_display = ("name", "slug", "start_point", "end_point", "travel_time")
+
+
+@admin.register(DateRoute)
+class DirectionAdmin(admin.ModelAdmin):
+    """Класс для отображения на панели администратора информации о конкретной Дате, определенного Направления."""
+
+    list_display = ("data_route", "slug", "direction_name", "amount_trips")
+
+
+@admin.register(TimeTrip)
+class DirectionAdmin(admin.ModelAdmin):
+    """Класс для отображения на панели администратора информации о конкретной Дате, определенного Направления."""
+
+    list_display = ("departure_time", "date_of_the_trip", "carrier", "number_of_seats")
--- final_edu_project/travel_service/booking/models.py
+++ final_edu_project/travel_service/booking/models.py
@@ -12,11 +12,9 @@ class Direction(models.Model):
 
     name = models.CharField(max_length=70, verbose_name="Direction Name")
     slug = models.SlugField(max_length=70, verbose_name="Short Name")
-    start_point = models.CharField(max_length=40, verbose_name="Where does the route start")
+    start_point = models.CharField(max_length=40, verbose_name="Where does the route start?")
     end_point = models.CharField(max_length=40, verbose_name="Where does the route end?")
-    direction_date = models.DateField(
-        verbose_name="Date of all trips", auto_now_add=False
-    )
+    travel_time = models.IntegerField(default=0, verbose_name="Travel time in minutes")
 
     class Meta:
         verbose_name = "Direction"
@@ -24,17 +22,19 @@ class Direction(models.Model):
 
     def __str__(self):
         """Возвращает удобочитаемую строку для каждого объекта."""
+
         return f"{self.name}"
 
     @classmethod
     def get_default_direction_pk(cls):
         """Необходим для значений по default"""
+
         direction, created = cls.objects.get_or_create(
             name="Default",
             slug="default",
             start_point="default",
             end_point="default",
-            direction_date=datetime.date(),
+            travel_time=0,
         )
         return direction.pk
 
@@ -68,6 +68,7 @@ class DateRoute(models.Model):
 
     def __str__(self):
         """Возвращает удобочитаемую строку для каждого объекта."""
+
         return f"{self.data_route}"
 
     def max_min_price(self):
@@ -75,3 +76,28 @@ class DateRoute(models.Model):
         ...
 
 
+class TimeTrip(models.Model):
+    """Класс для создания модели - Время начала поездки"""
+
+    departure_time = models.TimeField(verbose_name="Departure time")
+    date_of_the_trip = models.ForeignKey(DateRoute, verbose_name="Travel date", on_delete=models.CASCADE)
+    carrier = models.CharField(max_length=80, verbose_name="Carrier Name")
+    number_of_seats = models.IntegerField(default=0, verbose_name="Number of seats")
+
+    class Meta:
+        verbose_name = "TimeTrip"
+        verbose_name_plural = "TimeTrips"
+
+    def __str__(self):
+        """Возвращает удобочитаемую строку для каждого объекта."""
+
+        return f"{self.departure_time}"
+
+    def number_of_reserved_places(self):
+        """Функция, предназначенная для подсчета количества забронированных мест пользователями
+        на определенное дату и время"""
+        ...
+
+    def arrival_time_calculation(self):
+        """Функция, предназначенная для подсчета времени прибытия"""
+        ...
