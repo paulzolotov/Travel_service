32f267f6ea98b893e79018ff3b7ab6d73839d7cf
paulzolotov <zolotov10pl@gmail.com>
2023-04-03 13:12:58 +0300

    add postgress_db

--- final_edu_project/travel_service/travel_service/settings.py
+++ final_edu_project/travel_service/travel_service/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'travel_service.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "travel_service_db",
+        "USER": "pavel3",
+        "PASSWORD": "password98",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
