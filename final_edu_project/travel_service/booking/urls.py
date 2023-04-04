0ab27dc45c716ef07e9a2dd4ad20534c44a8d4ef
paulzolotov <zolotov10pl@gmail.com>
2023-04-04 11:32:49 +0300

    add base templates and index.html

--- final_edu_project/travel_service/booking/urls.py
+++ final_edu_project/travel_service/booking/urls.py
@@ -5,8 +5,9 @@ from . import views
 app_name = "booking"
 
 urlpatterns = [
-    path("", views.order_index, name="index"),
-    path("<slug:direction>", views.order_index, name="direction"),
-    path("<slug:direction>/<slug:day>", views.order_index, name="rout")
+    path("", views.index, name="index"),
+    # path("<slug:direction>", views.get_direction, name="direction"),
+    # path("<slug:direction>/<slug:day>", views.order_index, name="rout"),
+    # path("contacts", views.contacts, name="contacts"),
+    # path("account", views.account, name="account"),
 ]
-
--- final_edu_project/travel_service/booking/views.py
+++ final_edu_project/travel_service/booking/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponseRedirect
+from .models import Direction, DateRoute, TimeTrip, Trip
 
 # Create your views here.
+
+
+def index(request: HttpRequest):
+    """Функция предназначена для перехода к странице со списком направлений"""
+    directions = Direction.objects.all()
+    return render(request, "booking/index.html", context={"directions": directions})
