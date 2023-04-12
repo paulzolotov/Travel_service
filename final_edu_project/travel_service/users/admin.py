d15657265386aca3043ca5917c1a3dc71c169256
paulzolotov <zolotov10pl@gmail.com>
2023-04-12 17:35:43 +0300

    add CustomUSerAdmin for demonstrate custom user fields from user models

--- final_edu_project/travel_service/users/admin.py
+++ final_edu_project/travel_service/users/admin.py
@@ -1,5 +1,26 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import BookingUser
+from .forms import CustomUserCreationForm
 
-admin.site.register(BookingUser, UserAdmin)
+
+class CustomUserAdmin(UserAdmin):
+    model = BookingUser
+    add_form = CustomUserCreationForm  # не подтянуло другие поля
+
+    fieldsets = (
+        *UserAdmin.fieldsets,
+        (
+            'User additional fields',
+            {
+                'fields': (
+                    'phone',
+                    'date_of_birth',
+                    'cookie_consent',
+                )
+            }
+        )
+    )
+
+
+admin.site.register(BookingUser, CustomUserAdmin)
